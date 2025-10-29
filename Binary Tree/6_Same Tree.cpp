4a440ee9956dc5c0ff1952f19526c7f305d97a58
Vladbori21 <Vladbori21@mail.ru>
2025-10-29 19:47:38 +0300

    6_Same Tree.cpp/code shorter

--- Binary Tree/6_Same Tree.cpp
+++ Binary Tree/6_Same Tree.cpp
@@ -22,18 +22,12 @@
 class Solution {
 public:
 
-    bool check(TreeNode* p, TreeNode* q){
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
         if(!p && !q) return true;
         if(!p && q || !q && p) return false;
         if(p->val != q->val) return false;
-        if(!check(p->left, q->left)) return false;
-        if(!check(p->right, q->right)) return false;
-        return true;
-    }
-
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-
-        return check(p, q);
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         
     }
 };
