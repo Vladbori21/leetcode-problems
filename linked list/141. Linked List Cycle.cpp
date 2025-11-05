1bc5f97665941edabc14c6cd58107203e136f790
Vladbori21 <Vladbori21@mail.ru>
2025-11-05 08:46:06 +0300

    Implement cycle detection in linked list

---
+++ linked list/141. Linked List Cycle.cpp
@@ -0,0 +1,23 @@
+//----------Fast(heap) and slow(turtle) technique -------------
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast, *slow;
+        fast = head;
+        slow = head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast = fast->next->next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+};
